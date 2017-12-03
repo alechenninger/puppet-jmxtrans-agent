1015d99fdee51e058d074140badd9285c1ce4610
Alec Henninger <alechenninger@gmail.com>
2017-12-03 13:34:36 -0500

    Puppetize jmxtrans-agent installation and config

---
+++ manifests/init.pp
@@ -0,0 +1,46 @@
+class jmxtrans_agent (
+  $version='1.2.7-SNAPSHOT',
+  $collection_interval_seconds=10,
+  $include_common_jvm_queries=true,
+  $additional_queries=[],
+  $graphite_host,
+  $graphite_name_prefix='collectd.#reversed_hostname#.jmx.',
+  $collection_interval_seconds=10,
+  $reload_configuration_check_interval_seconds=30,
+  $wait_for_mbean_server=true,
+  $owner,
+  $group,
+) {
+  # Not intended to be used outside the module.
+  $install_path = '/opt/jmxtrans_agent'
+  $config_path = '/etc/jmxtrans_agent/config.xml'
+  $jar_name = "jmxtrans-agent-$version.jar"
+  $jar_path = "$install_path/$jar_name"
+
+  # Intended to be used outside the module.
+  $jvm_arguments="-javaagent:$jar_path=$config_path -Djmxtrans.agent.premain.waitForMBeanServer=$wait_for_mbean_server"
+
+  file { $install_path:
+    ensure => directory,
+    owner  => $owner,
+    group  => $group,
+    mode   => '0644',
+  } ->
+  file { $jar_path:
+    source  => "puppet:///modules/jmxtrans_agent/$jar_name",
+    owner   => $owner,
+    group   => $group,
+  }
+
+  file { '/etc/jmxtrans_agent':
+    ensure => directory,
+    owner  => $owner,
+    group  => $group,
+    mode   => '0644',
+  } ->
+  file { $config_path:
+    content => template('jmxtrans_agent/config.xml.erb'),
+    owner   => $owner,
+    group   => $group,
+  }
+}
